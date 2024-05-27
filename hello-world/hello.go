b028aca45080b2949eaebb4225d98df6db906fde
Fabio Simoes <fabio.simoes@gmail.com>
2024-05-27 20:17:53 -0300

    ♻️ add new tests

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,10 +2,33 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name string, language string) string {
+
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World", "Spanish"))
 }
